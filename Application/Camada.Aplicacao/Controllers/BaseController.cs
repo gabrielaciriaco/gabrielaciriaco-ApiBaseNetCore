6fb6124cc03e01cb06c8174e7d59ad618ac43896
Gabriela Ciríaco - DTI <dti85@localiza.com>
2020-03-27 16:38:18 -0300

    Criação do controller da API

--- Application/Camada.Aplicacao/Controllers/BaseController.cs
+++ Application/Camada.Aplicacao/Controllers/BaseController.cs
@@ -1,15 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
+using Camada.Domain.Interfaces;
+using Camada.Service.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Camada.Aplicacao.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/TodoItem/[controller]")]
     [ApiController]
     public class BaseController : ControllerBase
     {
+        private readonly ServiceBase servicos;
+
+        public BaseController(ServiceBase servicos)
+        {
+            this.servicos = servicos;
+        }
+
+        [HttpPost]
+        [Route("inserirItem")]
+        public IActionResult InserirItem ([FromBody] int id, string nome, string estaCompleto)
+        {
+            var resultado = servicos.ExecutarInsert(new RItemEntidade()
+            {
+                Id = id,
+                Nome = nome,
+                EstaCompleto = estaCompleto
+            });
+
+            return new ObjectResult(resultado);
+        }
+
+        [HttpPut]
+        [Route("atualizarItem")]
+        public IActionResult atualizarItem([FromBody] int id, string nome, string estaCompleto)
+        {
+            var resultado = servicos.ExecutarUpdate(new RItemEntidade()
+            {
+                Id = id,
+                Nome = nome,
+                EstaCompleto = estaCompleto
+            });
+
+            return new ObjectResult(resultado);
+        }
+
+        [HttpPut]
+        [Route("deletarItem")]
+        public IActionResult deletarItem([FromBody] int id)
+        {
+            var resultado = servicos.ExecutarRemove(id); 
+            return new ObjectResult(resultado);
+        }
+
+        [HttpGet]
+        [Route("selecionarItem")]
+        public IActionResult selecionarItem([FromBody] int id)
+        {
+            var resultado = servicos.ExecutarSelect(id);
+            return new ObjectResult(resultado);
+        }
     }
 }
